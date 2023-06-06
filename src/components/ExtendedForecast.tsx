7abc8b8a6c9d10760cef354a8be7090ea421bfd9
Kelvis Delapena <kelvisdelapena@gmail.com>
2023-06-06 10:12:26 -0300

    feat: added some mock icons to show in the horizontal scroll

--- src/components/ExtendedForecast.tsx
+++ src/components/ExtendedForecast.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Surface, Text } from "@react-native-material/core";
+import { Box, Flex, Icon, Surface, Text } from "@react-native-material/core";
 import { ScrollView } from "react-native";
 
 const ExtendedForecast = () => {
@@ -16,13 +16,70 @@ const ExtendedForecast = () => {
       <Text color="white" style={{ fontSize: 26 }}>
         Extended Forecast
       </Text>
-      <ScrollView horizontal>
+      <ScrollView horizontal style={{ marginTop: 20 }}>
         <Flex items="center" mr={10}>
           <Text color="white" style={{ fontSize: 26 }}>
             Tue
           </Text>
+          <Icon name="weather-sunny" color="white" style={{ fontSize: 50 }} />
           <Text color="white" style={{ fontSize: 22 }}>
-            Clouds
+            Sunny
+          </Text>
+          <Text color="white" style={{ fontSize: 20 }}>
+            22°/11°
+          </Text>
+        </Flex>
+        <Flex items="center" mr={10}>
+          <Text color="white" style={{ fontSize: 26 }}>
+            WED
+          </Text>
+          <Icon name="weather-rainy" color="white" style={{ fontSize: 50 }} />
+          <Text color="white" style={{ fontSize: 22 }}>
+            Rainy
+          </Text>
+          <Text color="white" style={{ fontSize: 20 }}>
+            22°/11°
+          </Text>
+        </Flex>
+        <Flex items="center" mr={10}>
+          <Text color="white" style={{ fontSize: 26 }}>
+            THU
+          </Text>
+          <Icon name="weather-cloudy" color="white" style={{ fontSize: 50 }} />
+          <Text color="white" style={{ fontSize: 22 }}>
+            Cloudy
+          </Text>
+          <Text color="white" style={{ fontSize: 20 }}>
+            22°/11°
+          </Text>
+        </Flex>
+        <Flex items="center" mr={10}>
+          <Text color="white" style={{ fontSize: 26 }}>
+            FRI
+          </Text>
+          <Icon
+            name="weather-partly-cloudy"
+            color="white"
+            style={{ fontSize: 50 }}
+          />
+          <Text color="white" style={{ fontSize: 22 }}>
+            P/Cloudy
+          </Text>
+          <Text color="white" style={{ fontSize: 20 }}>
+            22°/11°
+          </Text>
+        </Flex>
+        <Flex items="center" mr={10}>
+          <Text color="white" style={{ fontSize: 26 }}>
+            SAT
+          </Text>
+          <Icon
+            name="weather-lightning-rainy"
+            color="white"
+            style={{ fontSize: 50 }}
+          />
+          <Text color="white" style={{ fontSize: 22 }}>
+            Lightning
           </Text>
           <Text color="white" style={{ fontSize: 20 }}>
             22°/11°
