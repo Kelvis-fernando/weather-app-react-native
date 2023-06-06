560ec015e7d2351ea50f85d44cd0898faad3a748
Kelvis Delapena <kelvisdelapena@gmail.com>
2023-06-06 09:56:23 -0300

    feat: added the search input to the app, and wrapper app with IconComponentProvider, now the app can use Icons

--- App.tsx
+++ App.tsx
@@ -1,14 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Box, Button } from "@react-native-material/core";
+import {
+  Box,
+  Button,
+  IconComponentProvider,
+} from "@react-native-material/core";
 import HomeScreen from "./src/screens/HomeScreen";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 export default function App() {
   return (
-    <Box>
-      <HomeScreen />
-      <StatusBar style="auto" />
-    </Box>
+    <IconComponentProvider IconComponent={MaterialCommunityIcons}>
+      <Box>
+        <HomeScreen />
+        <StatusBar style="auto" />
+      </Box>
+    </IconComponentProvider>
   );
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,21 @@
-import { Flex } from "@react-native-material/core";
+import { Flex, Icon, IconButton, TextInput } from "@react-native-material/core";
 import CurrentDayWeather from "../components/CurrentDayWeather";
 import ExtendedForecast from "../components/ExtendedForecast";
 
 const HomeScreen = () => {
   return (
-    <Flex bg="#031025" justify="center" items="center" h="100%" w="100%">
+    <Flex bg="#0a1c3a" justify="center" items="center" h="100%" w="100%">
+      <TextInput
+        placeholder="Search a place"
+        variant="outlined"
+        style={{ width: "90%", marginBottom: 20 }}
+        trailing={(props) => (
+          <IconButton
+            icon={(props) => <Icon name="crosshairs-gps" {...props} />}
+            {...props}
+          />
+        )}
+      />
       <CurrentDayWeather />
       <ExtendedForecast />
     </Flex>
